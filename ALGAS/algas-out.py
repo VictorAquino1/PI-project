37e736168afda0b029bd800c35f12299f4ab8693
Victor Aquino <victor.msaquino@gmail.com>
2023-04-25 00:57:37 -0300

    add querry azure db

--- ALGAS/algas-out.py
+++ ALGAS/algas-out.py
@@ -1,44 +1,14 @@
-print("==== EXPORTANDO BASE 'machineData' PARA CSV ====")
-mpg_hdr = ['id', 'time', 'space']
-mpg_query = ('SELECT id, time, space FROM machineData')
-cursor.execute(mpg_query)
-with open('machineData_Base.csv', 'w', newline='') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(mpg_hdr)
-    for (id, time, space) in cursor:
-        writer.writerow([id, time, space])
+import pyodbc
+import matplotlib.pyplot as plt
 
-print("==== EXPORTANDO BASE 'sensor' PARA CSV ====")
-mpg_hdr = ['id', 'valor', 'data']
-mpg_query = ('SELECT id, valor, data FROM sensor')
-cursor.execute(mpg_query)
-with open('sensor_Base.csv', 'w', newline='') as fp:
-    writer = csv.writer(fp)
-    writer.writerow(mpg_hdr)
-    for (id, valor, data) in cursor:
-        writer.writerow([id, valor, data])
+cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:gp05-monitoramento.database.windows.net,1433;Database=gp05-monitoramento;Uid=gp-05;Pwd={#Gf51451488874};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+cursor = cnxn.cursor()
+querry = "Select * from [dbo].[sensor];"
+count = cursor.execute(querry)
+valor = []
 
+for x in count:
+    l1.append(x.valor) 
 
-# GRAPHIC CONFIG
-
-            fig, (ax1, ax2) = plt.subplots(2)
-    fig.suptitle(f'Monitoramento do sensor de proximidade caso = {x}')
-    ax1.set_title('Dados de tempo')
-    ax1.set_xlabel('Quantidade')
-    ax1.set_ylabel('Tempo (s)')
-    ax2.set_title('Comportamento do tamanho do array')
-    ax2.set_xlabel('Quantidade')
-    ax2.set_ylabel('Tamanho do array')
-    ax1.plot(l1, "o--")
-    ax2.plot(l2, "x-")
-
-
- plt.show()
-
-    cursor.execute("SELECT valor FROM sensor")
-myresult = cursor.fetchall()
-fig, ax = plt.subplots()
-ax.plot(myresult)
-ax.set_xlabel("Quantidade")
-ax.set_ylabel("Distancia (mm)")
-plt.show()
+plt.plot(l1)
+plt.ylabel("Valores do sensor")
